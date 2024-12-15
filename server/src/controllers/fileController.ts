393cfc5a6d447de171f1e5d029b0b065a7d321fd
VanceGoblin <zvkubajak@gmail.com>
2024-12-15 10:28:21 -0600

    Setup New Routes

--- server/src/controllers/fileController.ts
+++ server/src/controllers/fileController.ts
@@ -1,13 +1,14 @@
 import { Request, Response } from "express";
 import { File } from "../models";
 
-export const uploadFile = async (req: any, res: any) => {
+export const uploadFile = async (req: Request, res: any) => {
   if (!req.file) {
     return res.status(400).send("No file uploaded.");
   }
 
   try {
     const newFile = await File.create({
+      userId: req.body.userId,
       fileName: req.file.originalname,
       fileSize: req.file.size,
       content: JSON.parse(req.file.buffer.toString()),
