0744d07e84d41ad8ab155613748213afcd6eb845
VanceGoblin <zvkubajak@gmail.com>
2024-12-15 09:00:18 -0600

    Created fileController.ts

---
+++ server/src/controllers/fileController.ts
@@ -0,0 +1,24 @@
+import { type Request, type Response } from "express";
+import { File } from "../models";
+
+export const uploadFile = async (req: Request, res: Response) => {
+  if (!req.file) {
+    return res.status(400).send("No file uploaded.");
+  }
+
+  try {
+    const newFile = await File.create({
+      fileName: req.file.originalname,
+      fileSize: req.file.size,
+      content: JSON.parse(req.file.buffer.toString()),
+      uploadedAt: new Date(),
+    });
+
+    res
+      .status(200)
+      .json({ message: "File uploaded successfully", file: newFile });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+};
