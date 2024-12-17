e251ff8670ea8be721f51d6869f0f11dc49060f1
VanceGoblin <zvkubajak@gmail.com>
2024-12-17 09:49:45 -0600

    Created fetchFiles.ts

---
+++ client/src/services/fetchFiles.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export const fetchFiles = async () => {
+  try {
+    const response = await axios.get(
+      `https://localhost:3001/file`
+    );
+
+    console.log("JSON Files:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching JSON files:", error);
+  }
+};
