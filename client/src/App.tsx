ad6940468a59e7829f033683565b3e4ee414b8ac
bryceberczik <berczikbryce@gmail.com>
2024-12-16 18:13:34 -0600

    completed basic home page / hardcoded sphere with minimal styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 import Footer from "./components/Footer";
 
 const App = () => {
@@ -7,7 +7,7 @@ const App = () => {
   return (
 
     <div>
-      <Header />
+      <Sidebar />
       <main>
       <Outlet />
       </main>
