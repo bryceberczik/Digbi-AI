12898c08a84f51d051e056222f2d7ad254c6c490
VanceGoblin <zvkubajak@gmail.com>
2024-12-18 12:01:59 -0600

    Added Route

--- server/src/routes/file/fileRoutes.ts
+++ server/src/routes/file/fileRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import upload from "../../config/upload";
-import { getFiles, uploadFile } from "../../controllers/fileController";
+import { getFiles, uploadFile, removeFile } from "../../controllers/fileController";
 
 const router = express.Router();
 
 router.get("/", getFiles);
 router.post("/upload", upload.single("file"), uploadFile);
+router.delete("/remove/:id", removeFile);
 
 export default router;
