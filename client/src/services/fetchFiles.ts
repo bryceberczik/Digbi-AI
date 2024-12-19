dcb4fc395384c302da638edd1aabb72460a935cc
VanceGoblin <zvkubajak@gmail.com>
2024-12-19 14:17:57 -0600

    Got Started on Frontend uploadFile Function

--- client/src/services/fetchFiles.ts
+++ client/src/services/fetchFiles.ts
@@ -2,12 +2,18 @@ import axios from "axios";
 
 export const fetchFiles = async (userId: string) => {
   try {
-    const response = await axios.get(
-      `http://localhost:3001/file/${userId}`
-    );
+    const response = await axios.get(`http://localhost:3001/file/${userId}`);
 
     return response.data;
   } catch (error) {
     console.error("Error fetching JSON files:", error);
+  }
+};
+
+export const uploadFile = async (file: any, userId: string) => {
+  try {
+    const response = await axios.get(`http://localhost:3001/file/upload`);
+  } catch (error) {
+    console.error("Error uploading JSON file:", error);
   }
 };
