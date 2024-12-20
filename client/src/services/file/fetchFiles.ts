c796b67dd8fe3ab8ca163a1d0ab8a7087367040d
VanceGoblin <zvkubajak@gmail.com>
2024-12-19 19:47:35 -0600

    Debugged Validation

--- client/src/services/file/fetchFiles.ts
+++ client/src/services/file/fetchFiles.ts
@@ -11,8 +11,7 @@ export const fetchFiles = async (userId: string) => {
 
 export const fetchAllFiles = async () => {
   try {
-    const response = await axios.get(`https://localhost:3001/file`);
-    console.log(response.data);
+    const response = await axios.get(`http://localhost:3001/file`);
     return response.data;
   } catch (error) {
     console.error("Error fetching all JSON files:", error);
