33e9025bbf91cd94fdb887abb7a40363b75de49e
VanceGoblin <zvkubajak@gmail.com>
2024-12-22 14:33:20 -0600

    Current Status

--- client/src/services/file/removeFile.ts
+++ client/src/services/file/removeFile.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const removeFile = async (fileId: string) => {
   try {
-    await axios.delete(`https://digbiai.com/remove/${fileId}`);
+    await axios.delete(`/remove/${fileId}`);
   } catch (error) {
     console.error("Error removing JSON file:", error);
   }
--- client/src/services/file/uploadFile.ts
+++ client/src/services/file/uploadFile.ts
@@ -6,7 +6,7 @@ export const uploadFile = async (file: File, userId: string) => {
     formData.append("file", file);
     formData.append("userId", userId);
 
-    await axios.post(`https://digbiai.com/file/upload`, formData, {
+    await axios.post(`/file/upload`, formData, {
       headers: {
         "Content-Type": "multipart/form-data",
       },
