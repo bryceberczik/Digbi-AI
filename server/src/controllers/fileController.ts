58fa244865fd865843f38885d49645611c923389
VanceGoblin <zvkubajak@gmail.com>
2024-12-22 10:51:47 -0600

    Updated fileController.ts

--- server/src/controllers/fileController.ts
+++ server/src/controllers/fileController.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from "express";
 import { File } from "../models";
-import path from "path";
-import fs from "fs";
+import AWS from "aws-sdk";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const s3 = new AWS.S3();
 
 export const getFiles = async (_req: Request, res: Response) => {
   try {
@@ -36,15 +40,20 @@ export const uploadFile = async (req: Request, res: any) => {
   }
 
   try {
-    const fileContent = fs.readFileSync(req.file.path, "utf-8");
+    const params = {
+      Bucket: process.env.BUCKET_NAME!,
+      Key: req.file.originalname,
+      Body: req.file.buffer,
+      ContentType: req.file.mimetype,
+    };
 
-    const parsedContent = JSON.parse(fileContent);
+    const data = await s3.upload(params).promise();
 
     const newFile = await File.create({
       userId: req.body.userId,
       fileName: req.file.originalname,
       fileSize: req.file.size,
-      content: parsedContent,
+      s3Url: data.Location,
       uploadedAt: new Date(),
     });
 
@@ -52,7 +61,7 @@ export const uploadFile = async (req: Request, res: any) => {
       .status(200)
       .json({ message: "File Uploaded Successfully", file: newFile });
   } catch (error) {
-    console.error(error);
+    console.error("Error uploading file to S3:", error);
     res.status(500).send("Server Error");
   }
 };
@@ -63,9 +72,12 @@ export const removeFile = async (req: Request, res: any) => {
   try {
     const file = await File.findByPk(id);
     if (file) {
-      const filePath = path.join(__dirname, "../../db/json", file.fileName);
+      const params = {
+        Bucket: process.env.BUCKET_NAME!,
+        Key: file.fileName,
+      };
 
-      fs.unlinkSync(filePath);
+      await s3.deleteObject(params).promise();
       await file.destroy();
 
       res.json({ message: "File deleted." });
