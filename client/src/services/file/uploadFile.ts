b566bd9f9f226cb1ccef6eb7f5ba0978c0cdbd5f
Bryce Berczik <bryceberczik.dev@gmail.com>
2024-12-26 14:08:19 -0600

    Update uploadFile.ts

--- client/src/services/file/uploadFile.ts
+++ client/src/services/file/uploadFile.ts
@@ -3,10 +3,10 @@ import axios from "axios";
 export const uploadFile = async (file: File, userId: string) => {
   try {
     const formData = new FormData();
-    console.log("LOGGING HERE:", formData);
+    
     formData.append("file", file);
     formData.append("userId", userId);
-    console.log("LOGGING HERE:", formData);
+    
     await axios.post(`/file/upload`, formData, {
       headers: {
         "Content-Type": "multipart/form-data",
