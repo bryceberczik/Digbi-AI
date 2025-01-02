070230750235f52ad7f8b9b14edfb3eba4c0d525
Bryce Berczik <bryceberczik.dev@gmail.com>
2025-01-01 19:28:18 -0600

    Update deleteUser.ts

--- client/src/services/deleteUser.ts
+++ client/src/services/deleteUser.ts
@@ -2,9 +2,21 @@ import axios from "axios";
 
 export const deleteUser = async (id: string) => {
   try {
-    const response = await axios.delete(`/api/users/${id}`);
+
+    const token = localStorage.getItem("id_token");
+    if (!token) {
+      throw new Error("Authorization token is missing");
+    }
+
+    const response = await axios.delete(`/api/users/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
     return response.data;
   } catch (error) {
     console.error("Error deleting user", error);
+    throw error;
   }
 };
