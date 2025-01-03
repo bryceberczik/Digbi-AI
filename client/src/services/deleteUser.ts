1d4fed0b531d82981e928876cc1ab4ea1f51aa4c
VanceGoblin <zvkubajak@gmail.com>
2025-01-03 07:55:25 -0600

    Updated Frontend deleteUser Logic

--- client/src/services/deleteUser.ts
+++ client/src/services/deleteUser.ts
@@ -1,18 +1,21 @@
 import axios from "axios";
 
-export const deleteUser = async (id: string) => {
+export const deleteUser = async (id: string, email: string) => {
   try {
-
     const token = localStorage.getItem("id_token");
     if (!token) {
       throw new Error("Authorization token is missing");
     }
 
-    const response = await axios.delete(`http://localhost:3001/api/users/${id}`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
+    const response = await axios.delete(
+      `http://localhost:3001/api/users/${id}`,
+      {
+        data: { email },
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
 
     return response.data;
   } catch (error) {
