737661fd24c33c5bcee2958564feb46b6afe36cc
berczikbryce <bryceberczik.dev@gmail.com>
2025-01-03 21:33:19 -0600

    added completed get request and post request

--- server/src/routes/api/talkRoutes.ts
+++ server/src/routes/api/talkRoutes.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { createTalk } from "../../controllers/talkController";
+import { createTalk, getTalk } from "../../controllers/talkController";
 
 const router = express.Router();
 
-router.post("/create", createTalk)
+router.get("/:id", getTalk);
+router.post("/create", createTalk);
 
 export { router as talkRouter };
