937ab3b4570a8144696e2ff7a8374034daaaab49
VanceGoblin <zvkubajak@gmail.com>
2025-01-04 14:39:03 -0600

    Worked on Second Part of generateTalk.ts

--- client/src/services/generateTalk.ts
+++ client/src/services/generateTalk.ts
@@ -16,6 +16,12 @@ export const generateTalk = async (source_url: string, input: string) => {
     );
 
     const videoId = postResponse.data.id;
+
+    const getResponse = await axios.get(
+      `http://localhost:3001/api/talks/${videoId}`
+    );
+
+    console.log(getResponse.data);
   } catch (error: any) {
     console.error("Error generating talk:", error.response?.data);
   }
