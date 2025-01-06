18c9bb348731604abeee3ad4a0e0885b4eb8f7c3
VanceGoblin <zvkubajak@gmail.com>
2025-01-05 21:28:52 -0600

    Setting up Frontend Service For Talk

--- client/src/services/generateTalk.ts
+++ client/src/services/generateTalk.ts
@@ -1,16 +1,21 @@
 import axios from "axios";
 
-export const generateTalk = (source_url: string, input: string) => {
+export const generateTalk = async (source_url: string, input: string) => {
   try {
     const script = {
       type: "text",
       input: input,
     };
 
-    axios.post(`http://localhost:3001/api/talks/create`, {
-      source_url,
-      script,
-    });
+    const response = await axios.post(
+      `http://localhost:3001/api/talks/create`,
+      {
+        source_url,
+        script,
+      }
+    );
+
+    const result = response.data;
   } catch (error: any) {
     console.error("Error generating talk:", error.response?.data);
   }
